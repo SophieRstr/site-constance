971ab09c2e2008cc96c9b107e4f52c22979b091c
SophieRstr <65679584+SophieRstr@users.noreply.github.com>
2024-05-27 14:39:41 +0200

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   const menuToggle = document.querySelector('.menu-toggle');
   const menuItems = document.querySelector('.menu-items');
 
-  menuToggle.addEventListener('click', function () {
-    menuItems.classList.toggle('active');
+  menuToggle.addEventListener('click', function() {
     menuToggle.classList.toggle('active');
+    menuItems.classList.toggle('active');
   });
 });
